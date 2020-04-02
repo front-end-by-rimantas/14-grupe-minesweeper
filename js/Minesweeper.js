3e80c1823af3d31dbb606c8915250367c12f6928
Rimantas <belauzas@yahoo.com>
2020-04-02 12:44:57 +0300

    Generating bombs list

--- js/Minesweeper.js
+++ js/Minesweeper.js
@@ -10,6 +10,8 @@ class Minesweeper {
         this.bombsPercentage = bombsPercentage;
         this.bombsCount = 1;
 
+        this.clickCount = 0;
+
         // this.cells = [];
 
         this.init();
@@ -67,11 +69,52 @@ class Minesweeper {
         this.DOMfield = this.DOM.querySelector('.field');
 
         for ( let i=0; i<this.width * this.height; i++ ) {
-            new Cell(i, this.DOMfield);
+            new Cell(i, this);
         }
+    }
+
+    createBombs( cellIndex ) {
+        console.log('CREATING BOMBS... except for '+cellIndex);
+        console.log('wanted bombs count: '+this.bombsCount);
+        let list = [];
+        let counter = 0;
+
+        // for ( let i=0; i<this.bombsCount; i++ ) {
+        //     const position = Math.floor( Math.random() * this.width * this.height );
+        //     if ( list.indexOf(position) === -1 && position !== cellIndex ) {
+        //         list.push( position );
+        //     } else {
+        //         // neuzskaitome ciklo iteracijos kaip sekmingos, tad finale, kartojame dar karta
+        //         i--;
+        //     }
+        // }
+
+        while ( list.length < this.bombsCount ) {
+            counter++;
+            const position = Math.floor( Math.random() * this.width * this.height );
+
+            if ( list.indexOf(position) === -1 && position !== cellIndex ) {
+                list.push( position );
+            }
+        }
+
+        console.log('reikejo:', this.bombsCount, 'bet sukosi:', counter);
+
+        console.log(list);
+    }
+
+    checkCell( cellIndex ) {
+        console.log('cell: '+cellIndex);
+        
+        if ( this.clickCount === 0 ) {
+            this.createBombs( cellIndex );
+        }
+        this.clickCount++;
     }
 }
 
-const game = new Minesweeper('#game', 10, 10, 15);
+// const game = new Minesweeper('#game', 10, 10, 15);
+const game = new Minesweeper('#game', 10, 10, 80);
+// const game = new Minesweeper('#game', 10, 1, 99);
 
 console.log(game);
